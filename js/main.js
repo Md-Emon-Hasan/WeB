12c40af5b305babf86527fd9a3719ce5c5d31b96
iconicemon <iconicemon01@gmail.com>
2022-02-15 00:25:52 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     $('.navbar-nav li a').click(function(){
+//         $('.navbar-nav li a').removeClass('active');
+//         $(this).addClass("active");
+//     });
+// });
+
